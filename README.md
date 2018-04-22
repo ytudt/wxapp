9c2024a0be39a0ef2a7d806e70e0dbb498335687
dongtongtong <dongtongtong@meituan.com>
2018-04-22 11:17:41 +0800

    电影列表上拉刷新

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 微信小程序
+
+## scss转为wxss
+
++ sudo gem install sass
++ sass -v
++ which sass(查看sass安装路径)
++ webstrom->preference->file watchers->点击新建watch(scss)
++ program 输入上面sass路径
++ arguments 输入 --no-cache --update $FileName$:$FileNameWithoutExtension$.wxss
++ Output paths to refresh 输入$FileNameWithoutExtension$.wxss
++ 报存 修改scss文件就会自动变更到wxss
--- pages/movie/index.js
+++ pages/movie/index.js
@@ -4,89 +4,72 @@
 
 Page({
   data: {
-    hiddenLoading: false,
-    src: 'https://p0.meituan.net/165.220/movie/75fc3610bfe91b6ad8ae5f186f1c2fd6294933.jpg',
-    // movieList: [{
-    //   "showInfo": "今天169家影院放映2425场",
-    //   "late": false,
-    //   "sn": 0,
-    //   "cnms": 0,
-    //   "snum": 326289,
-    //   "imax": true,
-    //   "showDate": "",
-    //   "src": "",
-    //   "ver": "2D/3D/IMAX 3D/中国巨幕/全景声",
-    //   "rt": "2018-04-13上映",
-    //   "sc": 9.0,
-    //   "img": "https://p0.meituan.net/165.220/movie/75fc3610bfe91b6ad8ae5f186f1c2fd6294933.jpg",
-    //   "dur": 108,
-    //   "nm": "狂暴巨兽",
-    //   "wish": 160742,
-    //   "3d": true,
-    //   "pn": 94,
-    //   "scm": "",
-    //   "preSale": 0,
-    //   "vd": "",
-    //   "dir": "布拉德·佩顿",
-    //   "star": "道恩·强森,娜奥米·哈里斯,玛琳·阿克曼",
-    //   "cat": "动作,冒险,科幻",
-    //   "time": "",
-    //   "id": 341624
-    // },
-    //   {
-    //     "showInfo": "今天168家影院放映1273场",
-    //     "late": false,
-    //     "sn": 0,
-    //     "cnms": 0,
-    //     "snum": 23085,
-    //     "imax": false,
-    //     "showDate": "",
-    //     "src": "",
-    //     "ver": "2D",
-    //     "rt": "本周五上映",
-    //     "sc": 8.5,
-    //     "img": "https://p0.meituan.net/165.220/movie/8b154402ef3ac5d59e4a89649f4f5190375170.jpg",
-    //     "dur": 96,
-    //     "nm": "21克拉",
-    //     "wish": 119444,
-    //     "3d": true,
-    //     "pn": 159,
-    //     "scm": "破产拜金女，重新寻伴侣",
-    //     "preSale": 0,
-    //     "vd": "",
-    //     "dir": "何念",
-    //     "star": "郭京飞,迪丽热巴,大鹏",
-    //     "cat": "喜剧,爱情",
-    //     "time": "",
-    //     "id": 344950
-    //   }],
     movieList: [],
+    isLoading: false,
+    loadFinished: false,
+    pageSize: 10,
   },
   onMovieClick(event){
-    console.log('detail');
     console.log(event.currentTarget.id);
     wx.navigateTo({
       url: '../movieDetail/index'
     })
   },
   onBuyClick(event){
-    console.log('buy');
     console.log(event.currentTarget.id);
     wx.navigateTo({
       url: '../movieBuy/index'
     })
   },
-  onLoad: function () {
+  onScrollBottom(){
+    let offset = this.data.movieList.length;
+    this.getMovieList(offset);
+  },
+  getMovieList(offset){
+    let data = this.data;
+    if(data.isLoading || data.loadFinished) return;
+    this.setData({
+      isLoading: true,
+    });
+    offset === 0 && (wx.showLoading({
+      title: '加载中~',
+    }));
+    const pageSize = data.pageSize;
     wx.request({
-      url: 'https://m.maoyan.com/movie/list.json?type=hot&offset=0&limit=10',
+      url: 'https://m.maoyan.com/movie/list.json',
       method: 'GET',
+      data: {
+        type: 'hot',
+        limit: pageSize,
+        offset: offset,
+      },
       success: (res) => {
-        // console.log(res.data.data.movies);
+        let movies = res.data.data.movies;
+        let loadFinished = movies.length < data.pageSize;
+        let movieList = data.movieList.concat(movies);
         this.setData({
-          hiddenLoading: res.data.data.movies.length,
-          movieList: res.data.data.movies,
+          loadFinished,
+          movieList,
         });
-      }
+      },
+      fail: () => {
+        setTimeout(() => {
+          offset === 0 && (wx.showToast({
+            title: '加载失败请稍后再试~',
+            icon: 'none',
+          }));
+        }, 100);
+
+      },
+      complete: () => {
+        wx.hideLoading();
+        this.setData({
+          isLoading: false,
+        });
+      },
     })
+  },
+  onLoad: function () {
+    this.getMovieList(0);
   },
 })
